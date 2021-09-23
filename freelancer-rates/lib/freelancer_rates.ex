d35077cfd24a9079900dd9271d367897b64bcdeb
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2021-09-23 17:01:19 -0400

    Freelancer Rates | Monthly rate
    Taking the verbose approach as `trunc(daily_rate(hourly_rate) * 22 |> apply_discount(discount))` is technically correct but harder for me to read personally.

--- freelancer-rates/lib/freelancer_rates.ex
+++ freelancer-rates/lib/freelancer_rates.ex
@@ -10,8 +10,11 @@ defmodule FreelancerRates do
     before_discount - before_discount * percentage
   end
 
+  @spec monthly_rate(number, number) :: integer
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    monthly_rate = daily_rate(hourly_rate) * 22
+    discounted_rate = apply_discount(monthly_rate, discount)
+    trunc(discounted_rate)
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
