eb0d571ec703c251b4aafe36f96b0818fb07cb2b
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2021-09-23 22:57:39 -0400

    Guessing Game | Make a guess condition
    Re-implement `compare` using multiple clause functions. I should've paid attention to the concepts sections in Exercism as it defined how this should've looked. It also took me a second to work out that guess needed to default to `:no_guess` as that hits our existing clause. I hate to say it but I feel like I'm learning more doing this over elixir koans and I fucking love the koans.

--- guessing-game/lib/guessing_game.ex
+++ guessing-game/lib/guessing_game.ex
@@ -1,11 +1,9 @@
 defmodule GuessingGame do
-  def compare(secret_number, guess) do
-    cond do
-      guess === secret_number -> "Correct"
-      guess === secret_number - 1 -> "So close"
-      guess === secret_number + 1 -> "So close"
-      guess > secret_number -> "Too high"
-      guess < secret_number -> "Too low"
-    end
-  end
+  def compare(secret_number, guess \\ :no_guess)
+  def compare(secret_number, guess) when guess === secret_number, do: "Correct"
+  def compare(secret_number, guess) when guess === secret_number - 1, do: "So close"
+  def compare(secret_number, guess) when guess === secret_number + 1, do: "So close"
+  def compare(_secret_number, guess) when guess === :no_guess, do: "Make a guess"
+  def compare(secret_number, guess) when guess > secret_number, do: "Too high"
+  def compare(secret_number, guess) when guess < secret_number, do: "Too low"
 end
