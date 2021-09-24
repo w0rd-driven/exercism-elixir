1b83c201fb7ad9c53534a85eeb27ad6c4f58cb08
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2021-09-23 21:27:46 -0400

    Pacman Rules | Lost
    Implement `lose?` function using boolean `not` and `and` operators. It took way too long to figure this out and it was staring me in the face. I assumed by the 3rd function reuse would happen.

--- pacman-rules/lib/rules.ex
+++ pacman-rules/lib/rules.ex
@@ -1,16 +1,17 @@
 defmodule Rules do
-  @spec eat_ghost?(any, any) :: any
+  @spec eat_ghost?(boolean, boolean) :: any
   def eat_ghost?(power_pellet_active, touching_ghost) do
-    power_pellet_active && touching_ghost
+    power_pellet_active and touching_ghost
   end
 
-  @spec score?(any, any) :: any
+  @spec score?(boolean, boolean) :: any
   def score?(touching_power_pellet, touching_dot) do
-    touching_power_pellet || touching_dot
+    touching_power_pellet or touching_dot
   end
 
+  @spec lose?(boolean, boolean) :: any
   def lose?(power_pellet_active, touching_ghost) do
-    # Please implement the lose?/2 function
+    not power_pellet_active and touching_ghost
   end
 
   def win?(has_eaten_all_dots, power_pellet_active, touching_ghost) do
