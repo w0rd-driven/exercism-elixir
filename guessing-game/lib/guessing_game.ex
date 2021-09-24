f66cba3db96304655913424a50b1f68a38bf4d28
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2021-09-23 22:38:01 -0400

    Guessing Game | So close condition
    Implement `compare` using the conditionals. Order of operations matter as we need to capture +-1 before the greater or less than comparisons.

--- guessing-game/lib/guessing_game.ex
+++ guessing-game/lib/guessing_game.ex
@@ -2,6 +2,8 @@ defmodule GuessingGame do
   def compare(secret_number, guess) do
     cond do
       guess === secret_number -> "Correct"
+      guess === secret_number - 1 -> "So close"
+      guess === secret_number + 1 -> "So close"
       guess > secret_number -> "Too high"
       guess < secret_number -> "Too low"
     end
