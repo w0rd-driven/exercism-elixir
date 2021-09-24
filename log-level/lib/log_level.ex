a4e12a6cfcf28e38db05b842e0482f9144c4a847
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2021-09-23 22:01:10 -0400

    Log Level | To label
    Implement `to_label` function

--- log-level/lib/log_level.ex
+++ log-level/lib/log_level.ex
@@ -1,6 +1,15 @@
 defmodule LogLevel do
+  @spec to_label(any, any) :: :debug | :error | :fatal | :info | :trace | :unknown | :warning
   def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+    cond do
+      level == 0 && !legacy? -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5 && !legacy? -> :fatal
+      true -> :unknown
+    end
   end
 
   def alert_recipient(level, legacy?) do
