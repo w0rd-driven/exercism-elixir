402d7eb0295c8a1edd7738dca77a93baaf6accc0
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2021-09-27 17:56:05 -0400

    German sysadmin | Sanitize substitute final
    Turns out we don't need `to_charlist`. The exercism site alerted me that the purpose of the exercise is the following:
    ```
    The purpose of this exercise is to teach charlists. Solve it without using strings - do not use to_string, to_charlist, the String module, or the bitstring special form <<>>.
    ```

--- german-sysadmin/lib/username.ex
+++ german-sysadmin/lib/username.ex
@@ -8,10 +8,10 @@ defmodule Username do
     case head do
       head when head >= 0x0061 and head <= 0x0007A -> [head | sanitize(tail)]
       head when head == 0x005F -> [head | sanitize(tail)]
-      head when head == ?ä -> String.to_charlist("ae") ++ sanitize(tail)
-      head when head == ?ö -> String.to_charlist("oe") ++ sanitize(tail)
-      head when head == ?ü -> String.to_charlist("ue") ++ sanitize(tail)
-      head when head == ?ß -> String.to_charlist("ss") ++ sanitize(tail)
+      head when head == ?ä -> 'ae' ++ sanitize(tail)
+      head when head == ?ö -> 'oe' ++ sanitize(tail)
+      head when head == ?ü -> 'ue' ++ sanitize(tail)
+      head when head == ?ß -> 'ss' ++ sanitize(tail)
       _ -> sanitize(tail)
     end
   end
