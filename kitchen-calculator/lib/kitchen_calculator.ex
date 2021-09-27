f8c79bb9682bc70d28662b1088861f860dd5db7c
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2021-09-27 16:01:39 -0400

    Kitchen Calculator | To milliliter
    Implement `to_milliliter` using multi-clause functions and pattern matching.

--- kitchen-calculator/lib/kitchen_calculator.ex
+++ kitchen-calculator/lib/kitchen_calculator.ex
@@ -4,8 +4,35 @@ defmodule KitchenCalculator do
     elem(volume_pair, 1)
   end
 
+  @spec to_milliliter(
+          {:cup, number}
+          | {:fluid_ounce, number}
+          | {:milliliter, number}
+          | {:tablespoon, number}
+          | {:teaspoon, number}
+        ) :: {:milliliter, any}
   def to_milliliter(volume_pair) do
-    # Please implement the to_milliliter/1 functions
+    convert_to_milliliter(volume_pair)
+  end
+
+  defp convert_to_milliliter({:cup, volume}) do
+    {:milliliter, 240 * volume}
+  end
+
+  defp convert_to_milliliter({:fluid_ounce, volume}) do
+    {:milliliter, 30 * volume}
+  end
+
+  defp convert_to_milliliter({:teaspoon, volume}) do
+    {:milliliter, 5 * volume}
+  end
+
+  defp convert_to_milliliter({:tablespoon, volume}) do
+    {:milliliter, 15 * volume}
+  end
+
+  defp convert_to_milliliter({:milliliter, volume}) do
+    {:milliliter, volume}
   end
 
   def from_milliliter(volume_pair, unit) do
