fb5b945f08dcd1c7667115dbb4e34c9a68529487
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2021-09-27 23:37:48 -0400

    Darts |
    Implement `score` with outside target test passing

--- darts/lib/darts.ex
+++ darts/lib/darts.ex
@@ -6,5 +6,9 @@ defmodule Darts do
   """
   @spec score(position :: position) :: integer
   def score({x, y}) do
+    distance = :math.sqrt(x*x + y*y)
+    cond do
+      true -> 0
+    end
   end
 end
