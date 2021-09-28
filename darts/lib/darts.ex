7445e34ea42aaebfe4874656e911c67d2b9ba7a0
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2021-09-27 23:43:35 -0400

    Darts | Score inner circle
    Implement `score` with inner circle tests passing.

--- darts/lib/darts.ex
+++ darts/lib/darts.ex
@@ -10,6 +10,7 @@ defmodule Darts do
     cond do
       distance <= 10 and distance > 5 -> 1
       distance <= 5 and distance > 1 -> 5
+      distance <= 1 -> 10
       true -> 0
     end
   end
--- darts/test/darts_test.exs
+++ darts/test/darts_test.exs
@@ -16,22 +16,22 @@ defmodule DartsTest do
     assert Darts.score({-5, 0}) == 5
   end
 
-  @tag :pending
+  # @tag :pending
   test "On the inner circle" do
     assert Darts.score({0, -1}) == 10
   end
 
-  @tag :pending
+  # @tag :pending
   test "Exactly on centre" do
     assert Darts.score({0, 0}) == 10
   end
 
-  @tag :pending
+  # @tag :pending
   test "Near the centre" do
     assert Darts.score({-0.1, -0.1}) == 10
   end
 
-  @tag :pending
+  # @tag :pending
   test "Just within the inner circle" do
     assert Darts.score({0.7, 0.7}) == 10
   end
