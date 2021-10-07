546ed716cbd95f4da8492aef051b472b13b5f43c
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2021-10-06 21:45:09 -0400

    RPG Character Sheet | Variable input
    Implement `ask_name`, `ask_class`, and `ask_level` functions. They are identical save the conversion to an integer for level.

--- rpg-character-sheet/lib/rpg/character_sheet.ex
+++ rpg-character-sheet/lib/rpg/character_sheet.ex
@@ -5,16 +5,19 @@ defmodule RPG.CharacterSheet do
     :ok
   end
 
+  @spec ask_name :: String.t()
   def ask_name() do
-    # Please implement the ask_name/0 function
+    IO.gets("What is your character's name?\n") |> String.trim()
   end
 
+  @spec ask_class :: String.t()
   def ask_class() do
-    # Please implement the ask_class/0 function
+    IO.gets("What is your character's class?\n") |> String.trim()
   end
 
+  @spec ask_level :: integer()
   def ask_level() do
-    # Please implement the ask_level/0 function
+    IO.gets("What is your character's level?\n") |> String.trim() |> String.to_integer()
   end
 
   def run() do
