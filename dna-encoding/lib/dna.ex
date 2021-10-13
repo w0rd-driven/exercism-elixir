c5fe34c1cbcf519f934b5660eb6976f3e786d5fc
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2021-10-13 17:23:27 -0400

    DNA encoding | Decode the binary value to the nucleic acid
    Implement `decode_nucleotide/1` to decode binary code to nucleic acid, covering our second set of tests.

--- dna-encoding/lib/dna.ex
+++ dna-encoding/lib/dna.ex
@@ -10,7 +10,13 @@ defmodule DNA do
   end
 
   def decode_nucleotide(encoded_code) do
-    # Please implement the decode_nucleotide/1 function
+    case encoded_code do
+      0b0000 -> ?\s
+      0b0001 -> ?A
+      0b0010 -> ?C
+      0b0100 -> ?G
+      0b1000 -> ?T
+    end
   end
 
   def encode(dna) do
