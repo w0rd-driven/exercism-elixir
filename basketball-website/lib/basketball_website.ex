d442b77def83a15fe0587f90cb4b06352c4eff19
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2021-10-26 15:52:31 -0400

    Basketball Website | Extract data from a nested map structure
    Implement `extract_from_path/2` function using recursion.

--- basketball-website/lib/basketball_website.ex
+++ basketball-website/lib/basketball_website.ex
@@ -1,8 +1,14 @@
 defmodule BasketballWebsite do
+  @spec extract_from_path(any, String.t()) :: any
   def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
+    path_parts = String.split(path, ".")
+    extract_data(data, path_parts)
   end
 
+  defp extract_data(data, []), do: data
+  defp extract_data(data, [head | tail]) do
+    extract_data(data[head], tail)
+  end
   def get_in_path(data, path) do
     # Please implement the get_in_path/2 function
   end
