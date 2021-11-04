c1e16248d05f8c1bbf20347e58171b410093511d
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2021-11-04 19:14:18 -0400

    Newsletter | Log a sent email
    Implement `log_sent_email/2` function. Needed to use a function that takes in a pid and now I understand that's also the device to write too (like stderr?).

--- newsletter/lib/newsletter.ex
+++ newsletter/lib/newsletter.ex
@@ -1,16 +1,31 @@
 defmodule Newsletter do
+  @spec read_emails(
+          String.t()
+          | maybe_improper_list(
+              String.t() | maybe_improper_list(any, String.t() | []) | char,
+              String.t() | []
+            )
+        ) :: [String.t()]
   def read_emails(path) do
     {:ok, file} = File.read(path)
     file |> String.split(~r{\s}, trim: true)
   end
 
+  @spec open_log(
+          String.t()
+          | maybe_improper_list(
+              String.t() | maybe_improper_list(any, String.t() | []) | char,
+              String.t() | []
+            )
+        ) :: pid | {:file_descriptor, atom, any}
   def open_log(path) do
     {:ok, pid} = File.open(path, [:write])
     pid
   end
 
+  @spec log_sent_email(atom | pid, String.t()) :: :ok
   def log_sent_email(pid, email) do
-    # Please implement the log_sent_email/2 function
+    IO.write(pid, email <> "\n")
   end
 
   def close_log(pid) do
