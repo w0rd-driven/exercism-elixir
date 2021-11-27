982acd25f49b7b51de28131b320f08bd6ab11546
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2021-11-27 01:42:15 -0500

    File Sniffer | Verify that the file matches the expected type
    Implement `verify/2` using if statement

--- file-sniffer/lib/file_sniffer.ex
+++ file-sniffer/lib/file_sniffer.ex
@@ -1,4 +1,5 @@
 defmodule FileSniffer do
+  @spec type_from_extension(<<_::24>>) :: <<_::72, _::_*120>>
   def type_from_extension(extension) do
     case extension do
       "exe" -> "application/octet-stream"
@@ -9,6 +10,7 @@ defmodule FileSniffer do
     end
   end
 
+  @spec type_from_binary(<<_::16, _::_*8>>) :: <<_::72, _::_*120>>
   def type_from_binary(file_binary) do
     case file_binary do
       <<0x7F, 0x45, 0x4C, 0x46, _::binary>> -> "application/octet-stream"
@@ -19,7 +21,12 @@ defmodule FileSniffer do
     end
   end
 
+  @spec verify(<<_::16, _::_*8>>, <<_::24>>) :: {:error, <<_::424>>} | {:ok, <<_::72, _::_*120>>}
   def verify(file_binary, extension) do
-    # Please implement the verify/2 function
+    if type_from_binary(file_binary) == type_from_extension(extension) do
+      {:ok, type_from_binary(file_binary)}
+    else
+      {:error, "Warning, file format and file extension do not match."}
+    end
   end
 end
