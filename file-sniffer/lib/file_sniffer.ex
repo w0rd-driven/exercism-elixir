706cfe373d8afeef49abcbf69d1130e94ae31653
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2021-11-27 01:37:28 -0500

    File Sniffer | Given a binary file, return the expected media type
    Implement `type_from_binary/1` using case statement. Had to lookup how to include `_::binary` which is basically the equivalent to `_` as a variable.

--- file-sniffer/lib/file_sniffer.ex
+++ file-sniffer/lib/file_sniffer.ex
@@ -10,7 +10,13 @@ defmodule FileSniffer do
   end
 
   def type_from_binary(file_binary) do
-    # Please implement the type_from_binary/1 function
+    case file_binary do
+      <<0x7F, 0x45, 0x4C, 0x46, _::binary>> -> "application/octet-stream"
+      <<0x42, 0x4D, _::binary>> -> "image/bmp"
+      <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _::binary>> -> "image/png"
+      <<0xFF, 0xD8, 0xFF, _::binary>> -> "image/jpg"
+      <<0x47, 0x49, 0x46, _::binary>> -> "image/gif"
+    end
   end
 
   def verify(file_binary, extension) do
