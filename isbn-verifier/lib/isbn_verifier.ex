fe390fce34fe99fb546c78f70553d7c4a5793388
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2022-01-14 16:39:39 -0500

    ISBN Verifier | Initial commit

---
+++ isbn-verifier/lib/isbn_verifier.ex
@@ -0,0 +1,23 @@
+defmodule IsbnVerifier do
+  @doc """
+    Checks if a string is a valid ISBN-10 identifier
+
+    ## Examples
+
+      iex> IsbnVerifier.isbn?("3-598-21507-X")
+      true
+
+      iex> IsbnVerifier.isbn?("3-598-2K507-0")
+      false
+
+  """
+  @spec isbn?(String.t()) :: boolean
+  def isbn?(isbn) do
+  end
+
+  @spec get_isbn_as_list(String.t()) :: list
+  defp get_isbn_as_list(isbn) do
+    numbers = isbn |> String.replace("-", "", trim: true)
+
+  end
+end
