2f13af037d165a66b70a46c80baff9f2dd14dee3
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2022-02-16 14:47:17 -0500

    Boutique Suggestions | 3. Filter by combination price
    Filtering out further by combination price

--- boutique-suggestions/lib/boutique_suggestions.ex
+++ boutique-suggestions/lib/boutique_suggestions.ex
@@ -1,8 +1,11 @@
 defmodule BoutiqueSuggestions do
-  def get_combinations(tops, bottoms, _options \\ []) do
+  def get_combinations(tops, bottoms, options \\ []) do
+    maximum_price = Keyword.get(options, :maximum_price, 100)
+
     for top <- tops,
         bottom <- bottoms,
-        top.base_color != bottom.base_color do
+        top.base_color != bottom.base_color,
+        top.price + bottom.price <= maximum_price do
       {top, bottom}
     end
   end
