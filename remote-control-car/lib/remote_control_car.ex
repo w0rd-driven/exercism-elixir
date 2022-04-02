08bb1153ea90be456e4d6c75094f3bfca80102ab
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2022-04-02 00:08:18 -0400

    Remote Control Car | Refactor
    I knew I could specify defaults so I refactored to do that, including the %__MODULE__ syntax, making it much more concise. I need to take up this habit more as it reduces the need for %RemoteControlCar everywhere.

--- remote-control-car/lib/remote_control_car.ex
+++ remote-control-car/lib/remote_control_car.ex
@@ -1,33 +1,30 @@
 defmodule RemoteControlCar do
   @enforce_keys [:nickname]
-  defstruct [:battery_percentage, :distance_driven_in_meters, :nickname]
+  defstruct [:nickname, battery_percentage: 100, distance_driven_in_meters: 0]
 
-  @spec new :: %RemoteControlCar{
+  @spec new :: %__MODULE__{
           battery_percentage: integer(),
           distance_driven_in_meters: integer(),
           nickname: String.t()
         }
   def new() do
-    %RemoteControlCar{
-      battery_percentage: 100,
-      distance_driven_in_meters: 0,
-      nickname: "none"
-    }
+    %__MODULE__{nickname: "none"}
   end
 
-  @spec new(any) :: %RemoteControlCar{
+  @spec new(any) :: %__MODULE__{
           battery_percentage: integer(),
           distance_driven_in_meters: integer(),
           nickname: String.t()
         }
   def new(nickname) do
-    %RemoteControlCar{
-      battery_percentage: 100,
-      distance_driven_in_meters: 0,
-      nickname: nickname
-    }
+    %__MODULE__{nickname: nickname}
   end
 
+  @spec display_distance(%__MODULE__{
+          :battery_percentage => integer(),
+          :distance_driven_in_meters => integer,
+          :nickname => String.t()
+        }) :: String.t()
   def display_distance(remote_car = %RemoteControlCar{}) do
     "#{remote_car.distance_driven_in_meters} meters"
   end
