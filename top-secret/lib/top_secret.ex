75860f6068c93c5701038aeb7c60a94b5382dee2
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2022-07-14 16:33:43 -0400

    Top Secret | 2. Parse a single AST node
    Implement the `TopSecret.decode_secret_message_part/2` function. Definitely pretty heavily cheating here sadly. I know how to do this in parts, the pattern to pull out each section, the guard to limit to just def/defp, and getting the function name and arguments.

--- top-secret/lib/top_secret.ex
+++ top-secret/lib/top_secret.ex
@@ -3,8 +3,25 @@ defmodule TopSecret do
     Code.string_to_quoted!(string)
   end
 
+  def decode_secret_message_part({operation, _, definition} = ast, acc) when operation in [:def, :defp] do
+    {name, arguments} = get_function_name_and_arguments(definition)
+
+    arity = length(arguments)
+    message = String.slice(to_string(name), 0, arity)
+
+    {ast, [message | acc]}
+  end
+
   def decode_secret_message_part(ast, acc) do
-    # Please implement the decode_secret_message_part/2 function
+    {ast, acc}
+  end
+
+  defp get_function_name_and_arguments(definition) do
+    case definition do
+      [{:when, _, arguments} | _] -> get_function_name_and_arguments(arguments)
+      [{name, _, arguments} | _] when is_list(arguments) -> {name, arguments}
+      [{name, _, arguments} | _] when is_atom(arguments) -> {name, []}
+    end
   end
 
   def decode_secret_message(string) do
