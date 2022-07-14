babf3e781055af29652b9706d118b79a1a886e7f
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2022-07-14 14:39:15 -0400

    Bread And Potions | 3. Make mana potions edible
    Implement the `RPG.Edible` protocol for the `RPG.ManaPotion` item. Checked my work as with the last commit

--- bread-and-potions/lib/rpg.ex
+++ bread-and-potions/lib/rpg.ex
@@ -20,13 +20,19 @@ defmodule RPG do
   end
 
   defprotocol Edible do
-    @doc "Character consuming the item, give the effects"
+    @doc "Character consuming the item, display the effects"
     def eat(item, character)
   end
 
   defimpl Edible, for: LoafOfBread do
     def eat(_item, character) do
       {nil, %{character | health: character.health + 5}}
+    end
+  end
+
+  defimpl Edible, for: ManaPotion do
+    def eat(item, character) do
+      {%EmptyBottle{}, %{character | mana: character.mana + item.strength}}
     end
   end
 end
