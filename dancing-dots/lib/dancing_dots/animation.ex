7d300837494c50643cbbc9db9befc57058ec9b65
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2022-07-15 15:26:53 -0400

    Dancing Dots | 1. Define the animation behaviour
    Implement `init/1` and `handle_frame/3`.

--- dancing-dots/lib/dancing_dots/animation.ex
+++ dancing-dots/lib/dancing_dots/animation.ex
@@ -5,6 +5,8 @@ defmodule DancingDots.Animation do
   @type frame_number :: pos_integer
 
   # Please implement the module
+  @callback init(opts) :: {:ok, opts} | {:error, error}
+  @callback handle_frame(dot, frame_number, opts) :: dot
 end
 
 defmodule DancingDots.Flicker do
